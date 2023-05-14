76a3d19ff866e8476dd6418177b9ad3d49aa1919
Parul Sahni <Parul.Sahni@Geminisolutions.com>
2023-05-14 23:17:24 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularAsignemnt1';
+  title:string= 'AngularAsignemnt1';
+  ct:number=0;
+  show:boolean=false;
+  data:{itemno:number,itemname:string}[]=[];
+  showtxt(){
+    this.show=!this.show;
+  }
+  countclick(){
+    this.ct++;
+    console.log(this.ct);
+  }
+addvalue(inputtxt:any)
+  {
+    //console.log(inputtxt.value);
+    if(inputtxt.value=="")
+    {
+      alert("Empty String is not allowed");
+      return;
+    }
+
+    this.data.push({itemno:this.data.length+1,itemname:inputtxt.value});
+    inputtxt.value="";
+    console.log(this.data);
+
+  }
 }
